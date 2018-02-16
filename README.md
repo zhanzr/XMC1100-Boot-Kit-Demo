5e312d1b46d1f203b8eea4162b12e57484fb9b87
zhanzr <zhanzr@foxmail.com>
2018-02-16 17:08:36 +0800

    Coremark for XMC1100 bookit.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-Dhrystone for XMC1100 Bootkit board.
+Coremark for XMC1100 Bootkit board.
 
 ========================================
-#define RUN_NUMBER    300000
+#define ITERATIONS    2000
+#define    HZ    1000
 ARMCC 5.06 -O3 -Otime microlib
-Microseconds for one run through Dhrystone[56-15923]:    52.9
-Dhrystones per Second:                      18907.2
-========================================
-#define RUN_NUMBER    300000
-ARMCC 5.06 -O3 -Otime stdlib
-Microseconds for one run through Dhrystone[22-7289]:    24.2
-Dhrystones per Second:                      41282.5
+CoreMark 1.0 : 40.287654 / ARMCC 5060422 --c99 -c --cpu Cortex-M0 -g -O3 -Otime --apcs=interwork --split_sections / STACK
 =======================================
+#define ITERATIONS    2000
+#define    HZ    1000
+ARMCC 5.06 -O3 -Otime stdlib
+CoreMark 1.0 : 40.420372 / ARMCC 5060422 --c99 -c --cpu Cortex-M0 -g -O3 -Otime --apcs=interwork --split_sections / STACK
+========================================
--- main.c
+++ main.c
@@ -17,9 +17,7 @@
 #include "led.h"
 #include "lcd2004.h"
 #include "flash_ecc.h"
-#include "dhry.h"
-
-#define RUN_NUMBER	300000
+#include "core_portme.h"
 
 #define UART_RX P1_3
 #define UART_TX P1_2
@@ -944,153 +942,9 @@ uint8_t line[4][21] = {
 	"through the ITM"
 };
 
-
-/* Global Variables: */
-
-Rec_Pointer     Ptr_Glob,
-                Next_Ptr_Glob;
-int             Int_Glob;
-Boolean         Bool_Glob;
-char            Ch_1_Glob,
-                Ch_2_Glob;
-int             Arr_1_Glob [50];
-int             Arr_2_Glob [50] [50];
-
-#define REG	register
-	
-#ifndef REG
-        Boolean Reg = false;
-#define REG
-        /* REG becomes defined as empty */
-        /* i.e. no register variables   */
-#else
-        Boolean Reg = true;
-#endif
-
-/* variables for time measurement: */
-
-#ifdef TIMES
-struct tms      time_info;
-extern  int     times (void);
-                /* see library function "times" */
-#define Too_Small_Time (2*HZ)
-                /* Measurements should last at least about 2 seconds */
-#endif
-#ifdef TIME
-extern long     time(long *);
-                /* see library function "time"  */
-#define Too_Small_Time 2
-                /* Measurements should last at least 2 seconds */
-#endif
-#ifdef MSC_CLOCK
-//extern clock_t clock(void);
-#define Too_Small_Time (2*HZ)
-#endif
-
-long            Begin_Time,
-                End_Time,
-                User_Time;
-float           Microseconds,
-                Dhrystones_Per_Second;
-
-/* end of variables for time measurement */
-
-
-void Proc_1 (Rec_Pointer Ptr_Val_Par)
-/******************/
-    /* executed once */
+//int main(void)
+void original_main(void)
 {
-  REG Rec_Pointer Next_Record = Ptr_Val_Par->Ptr_Comp;
-                                        /* == Ptr_Glob_Next */
-  /* Local variable, initialized with Ptr_Val_Par->Ptr_Comp,    */
-  /* corresponds to "rename" in Ada, "with" in Pascal           */
-
-  structassign (*Ptr_Val_Par->Ptr_Comp, *Ptr_Glob);
-  Ptr_Val_Par->variant.var_1.Int_Comp = 5;
-  Next_Record->variant.var_1.Int_Comp = Ptr_Val_Par->variant.var_1.Int_Comp;
-  Next_Record->Ptr_Comp = Ptr_Val_Par->Ptr_Comp;
-  Proc_3 (&Next_Record->Ptr_Comp);
-    /* Ptr_Val_Par->Ptr_Comp->Ptr_Comp == Ptr_Glob->Ptr_Comp */
-  if (Next_Record->Discr == Ident_1)
-    /* then, executed */
-  {
-    Next_Record->variant.var_1.Int_Comp = 6;
-    Proc_6 (Ptr_Val_Par->variant.var_1.Enum_Comp,
-           &Next_Record->variant.var_1.Enum_Comp);
-    Next_Record->Ptr_Comp = Ptr_Glob->Ptr_Comp;
-    Proc_7 (Next_Record->variant.var_1.Int_Comp, 10,
-           &Next_Record->variant.var_1.Int_Comp);
-  }
-  else /* not executed */
-    structassign (*Ptr_Val_Par, *Ptr_Val_Par->Ptr_Comp);
-} /* Proc_1 */
-
-
-void Proc_2 (One_Fifty *Int_Par_Ref)
-/******************/
-    /* executed once */
-    /* *Int_Par_Ref == 1, becomes 4 */
-{
-  One_Fifty  Int_Loc;
-  Enumeration   Enum_Loc;
-
-  Int_Loc = *Int_Par_Ref + 10;
-  do /* executed once */
-    if (Ch_1_Glob == 'A')
-      /* then, executed */
-    {
-      Int_Loc -= 1;
-      *Int_Par_Ref = Int_Loc - Int_Glob;
-      Enum_Loc = Ident_1;
-    } /* if */
-    while (Enum_Loc != Ident_1); /* true */
-} /* Proc_2 */
-
-
-void Proc_3 (Rec_Pointer *Ptr_Ref_Par)
-/******************/
-    /* executed once */
-    /* Ptr_Ref_Par becomes Ptr_Glob */
-{
-  if (Ptr_Glob != Null)
-    /* then, executed */
-    *Ptr_Ref_Par = Ptr_Glob->Ptr_Comp;
-  Proc_7 (10, Int_Glob, &Ptr_Glob->variant.var_1.Int_Comp);
-} /* Proc_3 */
-
-
-void Proc_4 (void) /* without parameters */
-/*******/
-    /* executed once */
-{
-  Boolean Bool_Loc;
-
-  Bool_Loc = Ch_1_Glob == 'A';
-  Bool_Glob = Bool_Loc | Bool_Glob;
-  Ch_2_Glob = 'B';
-} /* Proc_4 */
-
-
-void Proc_5 (void) /* without parameters */
-/*******/
-    /* executed once */
-{
-  Ch_1_Glob = 'A';
-  Bool_Glob = false;
-} /* Proc_5 */
-
-int main(void)
-{
-  One_Fifty       Int_1_Loc;
-  REG One_Fifty   Int_2_Loc;
-  One_Fifty       Int_3_Loc;
-  REG char        Ch_Index;
-  Enumeration     Enum_Loc;
-  Str_30          Str_1_Loc;
-  Str_30          Str_2_Loc;
-  REG int         Run_Index;
-  REG int         Number_Of_Runs;
-	
 	__IO uint32_t tmpTick;
 	__IO uint32_t deltaTick;
 	__IO uint32_t i=0;		
@@ -1098,7 +952,7 @@ int main(void)
 	__IO XMC_RTC_TIME_t now_rtc_time;
 
   /* System timer configuration */
-  SysTick_Config(SystemCoreClock / 1000);
+  SysTick_Config(SystemCoreClock / HZ);
 	
   /*Initialize the UART driver */
 	uart_tx.mode = XMC_GPIO_MODE_OUTPUT_PUSH_PULL_ALT7;
@@ -1114,7 +968,7 @@ int main(void)
 	XMC_GPIO_Init(UART_TX, &uart_tx);
   XMC_GPIO_Init(UART_RX, &uart_rx);
 	
-  printf ("Dhrystone For XMC1100 Bootkit by Automan @ Infineon BBS @%u Hz\n",
+  printf ("Coremark For XMC1100 Bootkit by Automan @ Infineon BBS @%u Hz\n",
 	SystemCoreClock	);
 	
 	//RTC
@@ -1130,239 +984,34 @@ int main(void)
 	
 	LED_Initialize();
 	
-	LCD_Initialize();
-	
-	LCD_displayL(0, 0, line[0]);
-	LCD_displayL(1, 0, line[1]);
-	LCD_displayL(2, 0, line[2]);
-	LCD_displayL(3, 0, line[3]);
-	
-  /* Initializations */
-  Next_Ptr_Glob = (Rec_Pointer) malloc (sizeof (Rec_Type));
-  Ptr_Glob = (Rec_Pointer) malloc (sizeof (Rec_Type));
-
-  Ptr_Glob->Ptr_Comp                    = Next_Ptr_Glob;
-  Ptr_Glob->Discr                       = Ident_1;
-  Ptr_Glob->variant.var_1.Enum_Comp     = Ident_3;
-  Ptr_Glob->variant.var_1.Int_Comp      = 40;
-  strcpy (Ptr_Glob->variant.var_1.Str_Comp,
-          "DHRYSTONE PROGRAM, SOME STRING");
-  strcpy (Str_1_Loc, "DHRYSTONE PROGRAM, 1'ST STRING");
-
-  Arr_2_Glob [8][7] = 10;
-        /* Was missing in published program. Without this statement,    */
-        /* Arr_2_Glob [8][7] would have an undefined value.             */
-        /* Warning: With 16-Bit processors and Number_Of_Runs > 32000,  */
-        /* overflow may occur for this array element.                   */
-
-  printf ("\n");
-  printf ("Dhrystone Benchmark, Version 2.1 (Language: C)\n");
-  printf ("\n");
-  if (Reg)
-  {
-    printf ("Program compiled with 'register' attribute\n");
-    printf ("\n");
-  }
-  else
-  {
-    printf ("Program compiled without 'register' attribute\n");
-    printf ("\n");
-  }
-  printf ("Please give the number of runs through the benchmark: ");
-  {
-//    int n = 100000;
-//    scanf ("%d", &n);
-    Number_Of_Runs = RUN_NUMBER;
-  }
-  printf ("\n");
-
-  printf("Execution starts, %d runs through Dhrystone\n", Number_Of_Runs);
-  /***************/
-  /* Start timer */
-  /***************/
-
-#ifdef TIMES
-  times (&time_info);
-  Begin_Time = (long) time_info.tms_utime;
-#endif
-#ifdef TIME
-  Begin_Time = time ( (long *) 0);
-#endif
-#ifdef MSC_CLOCK
-  Begin_Time = g_Ticks;
-#endif
-
-  for (Run_Index = 1; Run_Index <= Number_Of_Runs; ++Run_Index)
-  {
-
-    Proc_5();
-    Proc_4();
-      /* Ch_1_Glob == 'A', Ch_2_Glob == 'B', Bool_Glob == true */
-    Int_1_Loc = 2;
-    Int_2_Loc = 3;
-    strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 2'ND STRING");
-    Enum_Loc = Ident_2;
-    Bool_Glob = ! Func_2 (Str_1_Loc, Str_2_Loc);
-      /* Bool_Glob == 1 */
-    while (Int_1_Loc < Int_2_Loc)  /* loop body executed once */
-    {
-      Int_3_Loc = 5 * Int_1_Loc - Int_2_Loc;
-        /* Int_3_Loc == 7 */
-      Proc_7 (Int_1_Loc, Int_2_Loc, &Int_3_Loc);
-        /* Int_3_Loc == 7 */
-      Int_1_Loc += 1;
-    } /* while */
-      /* Int_1_Loc == 3, Int_2_Loc == 3, Int_3_Loc == 7 */
-    Proc_8 (Arr_1_Glob, Arr_2_Glob, Int_1_Loc, Int_3_Loc);
-      /* Int_Glob == 5 */
-    Proc_1 (Ptr_Glob);
-    for (Ch_Index = 'A'; Ch_Index <= Ch_2_Glob; ++Ch_Index)
-                             /* loop body executed twice */
-    {
-      if (Enum_Loc == Func_1 (Ch_Index, 'C'))
-         /* then, not executed */
-      {
-        Proc_6 (Ident_1, &Enum_Loc);
-        strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 3'RD STRING");
-        Int_2_Loc = Run_Index;
-        Int_Glob = Run_Index;
-      }
-    }
-      /* Int_1_Loc == 3, Int_2_Loc == 3, Int_3_Loc == 7 */
-    Int_2_Loc = Int_2_Loc * Int_1_Loc;
-    Int_1_Loc = Int_2_Loc / Int_3_Loc;
-    Int_2_Loc = 7 * (Int_2_Loc - Int_3_Loc) - Int_1_Loc;
-      /* Int_1_Loc == 1, Int_2_Loc == 13, Int_3_Loc == 7 */
-    Proc_2 (&Int_1_Loc);
-      /* Int_1_Loc == 5 */
-
-  } /* loop "for Run_Index" */
-
-  /**************/
-  /* Stop timer */
-  /**************/
-
-#ifdef TIMES
-  times (&time_info);
-  End_Time = (long) time_info.tms_utime;
-#endif
-#ifdef TIME
-  End_Time = time ( (long *) 0);
-#endif
-#ifdef MSC_CLOCK
-  End_Time = g_Ticks;
-#endif
-
-  printf ("Execution ends\n");
-  printf ("\n");
-  printf ("Final values of the variables used in the benchmark:\n");
-  printf ("\n");
-  printf( "Int_Glob:            %d\n", Int_Glob);
-	printf("        should be:   %d\n", 5);
-	printf( "Bool_Glob:           %d\n", Bool_Glob);
-	printf( "        should be:   %d\n", 1);
-	printf("Ch_1_Glob:           %c\n", Ch_1_Glob);
-	printf("        should be:   %c\n", 'A');	
-  printf("Ch_2_Glob:           %c\n", Ch_2_Glob);	
-  printf("        should be:   %c\n", 'B');	
-  printf("Arr_1_Glob[8]:       %d\n", Arr_1_Glob[8]);	
-  printf("        should be:   %d\n", 7);	
-  printf("Arr_2_Glob[8][7]:    %d\n", Arr_2_Glob[8][7]);	
-  printf ("        should be:   Number_Of_Runs + 10\n");
-  printf ("Ptr_Glob->\n");
-  printf("  Ptr_Comp:          %d\n", (int) Ptr_Glob->Ptr_Comp);	
-  printf ("        should be:   (implementation-dependent)\n");
-  printf("  Discr:             %d\n", Ptr_Glob->Discr);	
-	printf("        should be:   %d\n", 0);	
-  printf("  Enum_Comp:         %d\n", Ptr_Glob->variant.var_1.Enum_Comp);	
-	printf("        should be:   %d\n", 2);	
-  printf("  Int_Comp:          %d\n", Ptr_Glob->variant.var_1.Int_Comp);	
-  printf("        should be:   %d\n", 17);	
-  printf("  Str_Comp:          %s\n", Ptr_Glob->variant.var_1.Str_Comp);	
-  printf ("        should be:   DHRYSTONE PROGRAM, SOME STRING\n");
-  printf ("Next_Ptr_Glob->\n");
-  printf("  Ptr_Comp:          %d\n", (int) Next_Ptr_Glob->Ptr_Comp);	
-  printf ("        should be:   (implementation-dependent), same as above\n");
-	printf("  Discr:             %d\n", Next_Ptr_Glob->Discr);	
-  printf("        should be:   %d\n", 0);	
-  printf("  Enum_Comp:         %d\n", Next_Ptr_Glob->variant.var_1.Enum_Comp);	
-  printf("        should be:   %d\n", 1);	
-  printf("  Int_Comp:          %d\n", Next_Ptr_Glob->variant.var_1.Int_Comp);	
-  printf("        should be:   %d\n", 18);
-  printf("  Str_Comp:          %s\n",
-                                Next_Ptr_Glob->variant.var_1.Str_Comp);	
-  printf ("        should be:   DHRYSTONE PROGRAM, SOME STRING\n");
-  printf("Int_1_Loc:           %d\n", Int_1_Loc);	
-  printf("        should be:   %d\n", 5);	
-  printf("Int_2_Loc:           %d\n", Int_2_Loc);	
-  printf("        should be:   %d\n", 13);	
-  printf("Int_3_Loc:           %d\n", Int_3_Loc);	
-  printf("        should be:   %d\n", 7);	
-  printf("Enum_Loc:            %d\n", Enum_Loc);	
-  printf("        should be:   %d\n", 1);	
-  printf("Str_1_Loc:           %s\n", Str_1_Loc);	
-  printf ("        should be:   DHRYSTONE PROGRAM, 1'ST STRING\n");
-  printf("Str_2_Loc:           %s\n", Str_2_Loc);	
-  printf ("        should be:   DHRYSTONE PROGRAM, 2'ND STRING\n");
-  printf ("\n");
-
-  User_Time = End_Time - Begin_Time;
-
-  if (User_Time < Too_Small_Time)
-  {
-		printf( "Measured time too small to obtain meaningful results %u-%u\n", Begin_Time, End_Time);
-    printf ("Please increase number of runs\n");
-  }
-  else
-  {
-#ifdef TIME
-    Microseconds = (float) User_Time * Mic_secs_Per_Second
-                        / (float) Number_Of_Runs;
-    Dhrystones_Per_Second = (float) Number_Of_Runs / (float) User_Time;
-#else
-    Microseconds = (float) User_Time * (float)Mic_secs_Per_Second
-                        / ((float) HZ * ((float) Number_Of_Runs));
-    Dhrystones_Per_Second = ((float) HZ * (float) Number_Of_Runs)
-                        / (float) User_Time;
-#endif
-		printf("Microseconds for one run through Dhrystone[%u-%u]:  ", Begin_Time, End_Time);
-
-    printf("%6.1f \n", Microseconds);
-	
-    printf ("Dhrystones per Second:                      ");
-    printf("%6.1f \n", Dhrystones_Per_Second);
-	
-  }
+//	LCD_Initialize();
+//	
+//	LCD_displayL(0, 0, line[0]);
+//	LCD_displayL(1, 0, line[1]);
+//	LCD_displayL(2, 0, line[2]);
+//	LCD_displayL(3, 0, line[3]);
 			
-	while (1)
-  {				
-//    LED_On(0);
-//    LED_On(1);
-//    LED_On(2);
-//    LED_On(3);
-    LED_On(4);
-		
-		tmpTick = g_Ticks;
-		while((tmpTick+2000) > g_Ticks)
-		{
-			__NOP();
-			__WFI();
-		}
-		
-		XMC_RTC_GetTime((XMC_RTC_TIME_t *)&now_rtc_time);
-//		printf("%02d:%02d:%02d\n", now_rtc_time.hours, now_rtc_time.minutes, now_rtc_time.seconds);
+//	while (1)
+//  {				
+//    LED_On(4);
+//		
+//		tmpTick = g_Ticks;
+//		while((tmpTick+2000) > g_Ticks)
+//		{
+//			__NOP();
+//			__WFI();
+//		}
+//		
+//		XMC_RTC_GetTime((XMC_RTC_TIME_t *)&now_rtc_time);
+////		printf("%02d:%02d:%02d\n", now_rtc_time.hours, now_rtc_time.minutes, now_rtc_time.seconds);
 
-//    LED_Off(0);
-//    LED_Off(1);
-//    LED_Off(2);
-//    LED_Off(3);
-    LED_Off(4);
-		
-		tmpTick = g_Ticks;
-		while((tmpTick+2000) > g_Ticks)
-		{
-			__NOP();
-			__WFI();
-		}		
-  }
+//    LED_Off(4);
+//		
+//		tmpTick = g_Ticks;
+//		while((tmpTick+2000) > g_Ticks)
+//		{
+//			__NOP();
+//			__WFI();
+//		}		
+//  }
 }
