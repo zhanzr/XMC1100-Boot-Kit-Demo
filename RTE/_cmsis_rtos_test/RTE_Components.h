2c8c64d05f485a559607f2788298dd9b307cc227
zhanzr <zhanzr@foxmail.com>
2018-10-03 18:39:05 +0800

    AES Test maintaince.

---
+++ RTE/_cmsis_rtos_test/RTE_Components.h
@@ -0,0 +1,24 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'cstyle_template' 
+ * Target:  'cmsis_rtos_test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "XMC1100.h"
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+
+#endif /* RTE_COMPONENTS_H */
