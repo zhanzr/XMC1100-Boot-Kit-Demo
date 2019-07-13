e16d9d55cc0748a6b2cbf0a1870d99b7d547bed4
zhanzr <zhanzr@foxmail.com>
2019-07-13 13:44:10 +0800

    maintaince

---
+++ RTE/_cmsis_rtos_test/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'cstyle_template' 
+ * Target:  'cmsis_rtos_test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "XMC1100.h"
+
+/*  ARM::CMSIS:RTOS:Keil RTX:4.81.1 */
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+
+
+#endif /* RTE_COMPONENTS_H */
